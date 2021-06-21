aa4bc6b18714f436c9fc00d323523f5ad592bb84
shivanix <shvni.here@gmail.com>
2021-06-21 23:14:07 +0000

    Added a new function that returns everything except the 1st item as an array

---
+++ tail.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const tail = function(input) {
+  return input.splice(1, input.length);
+};
+// console.log(tail([5,6,7]));
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `☠ 🚫 Assertion Failed: ${arguments[0]} !== ${arguments[1]}`;
+  } else {
+    return `😎😎 Assertion Passed: ${arguments[0]} === ${arguments[1]}`;
+  }
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"

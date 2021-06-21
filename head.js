228d4b9ab613efc426e411a2a65d6058c47c8967
shivanix <shvni.here@gmail.com>
2021-06-21 22:02:35 +0000

    Created function head to return 1st iem in the array to be used in custom assertion function

---
+++ head.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const head = function(item1) {
+  return item1[0];
+};
+// console.log(head([5,6,7], 5));
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `☠ 🚫 Assertion Failed: ${arguments[0]} !== ${arguments[1]}`;
+  } else {
+    return `😎😎 Assertion Passed: ${arguments[0]} === ${arguments[1]}`;
+  }
+};
+
+// TEST CODE
+console.log(assertEqual(head([5, 6, 7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));

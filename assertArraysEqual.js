65650853ea4f5d880d916e9dcf816e5aefc4bb48
shivanix <shvni.here@gmail.com>
2021-06-23 12:35:49 +0000

    Created function that will take 2 arrays and console.log a message to console

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,18 @@
 const assertArraysEqual = function(array1, array2) {
 
   if (array1.length !== array2.length) {
-    return `🚫☠ ${array1} is NOT equal to ${array2}`;
+    console.log(`🚫☠ ${array1} is NOT equal to ${array2}`);
+    return;
   }
 
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return `🚫☠ ${array1} is NOT equal to ${array2}`;
+      console.log(`🚫☠ ${array1} is NOT equal to ${array2}`);
+      break;
     }
   }
 
-  return `✔✔ ${array1} is Equal to ${array2}`;
+  console.log(`✔✔ ${array1} is Equal to ${array2}`);
 };
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false)
-console.log(assertArraysEqual([1, 2], [3, 2, 1])); // => false)
 
+assertArraysEqual([1,2,3], [1,4]);

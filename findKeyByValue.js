9e0ad16484e42532ac7d6aad07e36ef376ff7712
shivanix <shvni.here@gmail.com>
2021-06-24 01:07:40 +0000

    created f to scan object and return 1st key containing given value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `☠ 🚫 Assertion Failed: ${arguments[0]} !== ${arguments[1]}`;
+  }
+  return `😎😎 Assertion Passed: ${arguments[0]} === ${arguments[1]}`;
+
+};
+
+const findKeyByValue = function(sources, genre) {
+  for (let key of Object.keys(sources)) {
+    if (sources[key] === genre) {
+      return key;
+    }
+
+  }
+  return undefined;
+
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));

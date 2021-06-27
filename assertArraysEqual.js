535c78c3a7d6aa67d1944a4e7e2ab1355a40dc33
shivanix <shvni.here@gmail.com>
2021-06-27 02:02:26 +0000

    Used another function for array comparision inside assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,27 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
-    console.log(`🚫☠ ${array1} is NOT equal to ${array2}`);
-    return;
+    return false;
   }
 
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`🚫☠ ${array1} is NOT equal to ${array2}`);
-      break;
+      return false;
     }
+  }
+
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+
+  if (!eqArrays(array1, array2)) {
+    console.log(`🚫☠ ${array1} is NOT equal to ${array2}`);
+    return;
   }
 
   console.log(`✔✔ ${array1} is Equal to ${array2}`);
 };
 
-assertArraysEqual([1,2,3], [1,4]);
+assertArraysEqual([1, 2, 3], [1, 4]);

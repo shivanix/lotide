12ef3c7eca28fb870d242012ef931a3ca2b09b51
shivanix <shvni.here@gmail.com>
2021-06-28 20:28:50 +0000

    middleTest.js has been converted to use Mocha and Chai. assert.deepEqual was used

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual')
-const middle = require('../middle');
+const assert = require('chai').assert;
+const tail = require('../middle')
 
-//Test code
-console.log(assertArraysEqual(middle([1, 2, 4]),[2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2,3]));
-console.log(assertArraysEqual(middle([1, 2]),[]));
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 4]", () => {
+    assert.deepEqual(tail([1, 2, 4]), [2]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), []);
+  });
+  })
